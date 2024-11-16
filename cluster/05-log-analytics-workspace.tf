7e10c8f47c081ecda6167a259602013f36efc666
William Munoz <william@godeveloper.io>
2024-11-16 09:33:56 -0500

    validate cluster plan

--- cluster/05-log-analytics-workspace.tf
+++ cluster/05-log-analytics-workspace.tf
@@ -3,6 +3,6 @@ resource "azurerm_log_analytics_workspace" "insights" {
   location            = azurerm_resource_group.aks_rg.location
   name                = "logs-${random_pet.aksrandom.id}"
   resource_group_name = azurerm_resource_group.aks_rg.name
-  sku                 = "PerGB20218"
+  sku                 = "PerGB2018"
   retention_in_days   = 30
 }
