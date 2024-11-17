66fb6e7bc5a134e9c1730aa6db5e0490547ce1bf
William Munoz <william@godeveloper.io>
2024-11-17 14:02:17 -0500

    add aks cluster with azure devops

---
+++ cluster-azure-devops/07-aks-cluster.tf
@@ -0,0 +1,64 @@
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "${azurerm_resource_group.aks_rg.name}-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "${azurerm_resource_group.aks_rg.name}-cluster"
+  kubernetes_version  = data.azurerm_kubernetes_service_versions.current.latest_version
+  node_resource_group = "${azurerm_resource_group.aks_rg.name}-nrg"
+
+  default_node_pool {
+    name                 = "systempool"
+    vm_size              = "Standard_DS2_v2"
+    orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
+    zones = [1, 2, 3]
+    auto_scaling_enabled = true
+    max_count            = 3
+    min_count            = 1
+    os_disk_size_gb      = 30
+    type                 = "VirtualMachineScaleSets"
+    vnet_subnet_id       = azurerm_subnet.aks_default.id
+    node_labels = {
+      "nodepool-type"    = "system"
+      "environment"      = "dev"
+      "nodepoolos"       = "linux"
+      "app"              = "system-apps"
+    }
+    tags = {
+      "nodepool-type"    = "system"
+      "environment"      = "dev"
+      "nodepoolos"       = "linux"
+      "app"              = "system-apps"
+    }
+  }
+
+  # Identity (System Assigned or Service Principal)
+  identity {
+    type = "SystemAssigned"
+  }
+
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.insights.id
+  }
+
+  azure_active_directory_role_based_access_control {
+    admin_group_object_ids = [azuread_group.aks_administrators.object_id]
+  }
+
+  # Linux Profile
+  linux_profile {
+    admin_username = "ubuntu"
+    ssh_key {
+      key_data = file(var.ssh_public_key)
+    }
+  }
+
+  # Network Profile
+  network_profile {
+    network_plugin = "azure"
+    load_balancer_sku = "standard"
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
---
+++ cluster-azure-devops/09-aks-cluster-linux-user-nodepools.tf
@@ -0,0 +1,28 @@
+# Create Linux Azure AKS Node Pool
+resource "azurerm_kubernetes_cluster_node_pool" "linux_np" {
+  zones = [ 1, 2, 3 ]
+  auto_scaling_enabled = true
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id
+  max_count             = 3
+  min_count             = 1
+  mode                  = "User"
+  name                  = "linuxng"
+  orchestrator_version  = data.azurerm_kubernetes_service_versions.current.latest_version
+  os_disk_size_gb       = 30
+  os_type               = "Linux"
+  vm_size               = "Standard_DS2_v2"
+  priority              = "Regular"
+  vnet_subnet_id        = azurerm_subnet.aks_default.id
+  node_labels = {
+    "nodepool-type" = "user"
+    "environment"   = var.environment
+    "nodepoolos"    = "linux"
+    "app"           = "java-apps"
+  }
+  tags = {
+    "nodepool-type" = "user"
+    "environment"   = var.environment
+    "nodepoolos"    = "linux"
+    "app"           = "java-apps"
+  }
+}
