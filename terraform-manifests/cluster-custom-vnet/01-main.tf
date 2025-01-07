a28529aace7edb1b96104f1e25b8df053da52cfc
William Munoz <william@godeveloper.io>
2025-01-07 16:13:57 -0500

    allow to delete resource group if contains resources

--- terraform-manifests/cluster-custom-vnet/01-main.tf
+++ terraform-manifests/cluster-custom-vnet/01-main.tf
@@ -39,6 +39,9 @@ terraform {
 provider "azurerm" {
   subscription_id = var.subscription_id
   features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
   }
 }
 
